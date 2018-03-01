1b7261f9d13efb509cc80d08174bc0a00218a41d
Joseph Hamman <jhamman@ucar.edu>
2018-02-28 17:15:52 -0800

    update pangeo-aws, add dask-config.yaml

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,5 +1,7 @@
+set -x
+
 # Set up Kubernetes
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=mrocklin@gmail.com
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$EMAIL
 kubectl --namespace kube-system create sa tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
@@ -10,11 +12,11 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
 # Install JupyterHub and Dask on the cluster
-helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=pangeo -f jupyter-config.yaml
-helm install dask/dask --name=dask --namespace=pangeo -f dask-config.yaml
+helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=pangeo-aws -f jupyter-config.yaml
+helm install dask/dask --name=dask --namespace=pangeo-aws -f dask-config.yaml
 
 # Look for publised services.  For AWS, route CNAME records to load balancer public proxy
-kubectl get services --namespace pangeo
+kubectl get services --namespace pangeo-aws
 # Look for dask-scheduler
 #      and proxy-...
 
